c2e7e0832c30989929821291d845e05f17b222e3
vikasTmz <remote_user@brown.edu>
2021-09-30 02:25:54 -0400

    fsd

--- ShapeFlow/shapenet_reconstruct.py
+++ ShapeFlow/shapenet_reconstruct.py
@@ -57,6 +57,7 @@ def get_args():
         "--input_path",
         type=str,
         default="/media/andy/Elements/Shapeflow_data/data/shapenet_simplified/val/03001627/c4f9249def12870a2b3e9b6eb52d35df/model.ply",
+        # default="/media/andy/Elements/Shapeflow_data/data/shapenet_simplified/val/03001627/bcc73b8ff332b4df3d25ee35360a1f4d/model.ply",
         help="path to input points (.ply file).",
     )
     parser.add_argument(
@@ -136,7 +137,7 @@ def main():
     sample_points = 1024
     mesh_gt = trimesh.load(args_eval.input_path)
     mesh_v = np.array(mesh_gt.vertices)
-    points = mesh_gt.sample(sample_points)
+    # points = mesh_gt.sample(sample_points)
     print(mesh_v.shape)
     points = mesh_v[:1024]
     export_obj_cpu('shapenet_recon_input.obj', points, random_trans=[-1.5,0,0])
