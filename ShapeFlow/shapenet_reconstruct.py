48d4c8b0c7c6ac45c1831e90d659f557b6d2d34f
vikasTmz <remote_user@brown.edu>
2021-10-14 09:01:34 -0400

    updates

--- ShapeFlow/shapenet_reconstruct.py
+++ ShapeFlow/shapenet_reconstruct.py
@@ -140,7 +140,7 @@ def get_args():
         "-ne",
         "--embedding_niter",
         type=int,
-        default=30,
+        default=2,
         help="number of embedding iterations.",
     )
     parser.add_argument(
