89bc8c2901954caf2735813d05e79729c8b9c0a4
vikasTmz <remote_user@brown.edu>
2021-10-14 05:50:11 -0400

    updates

--- ShapeFlow/shapenet_reconstruct.py
+++ ShapeFlow/shapenet_reconstruct.py
@@ -115,7 +115,7 @@ def get_args():
     parser.add_argument(
         "--input_path_1",
         type=str,
-        default="3dfuture_5_pc.ply",
+        default="3dfuture_7_pc.ply",
         # default="/media/andy/Elements/Shapeflow_data/data/shapenet_simplified/val/03001627/c4f9249def12870a2b3e9b6eb52d35df/model.ply",
         # default="/media/andy/Elements/Shapeflow_data/data/shapenet_simplified/val/03001627/bcc73b8ff332b4df3d25ee35360a1f4d/model.ply",
         help="path to input points (.ply file).",
